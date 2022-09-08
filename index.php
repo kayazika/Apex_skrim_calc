db9bf0a744ba3538be8be5483085e62b608dc7b7
kaya <alexkayaalmeida@gmail.com>
2022-09-07 23:54:44 -0300

    base project commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style_base.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apex Skrim Calculator</title>
+</head>
+<body>
+    <header>
+        <div class="fLogo">
+            <img class="cLogo" src="img/logoApex.png" alt="">
+        </div>
+        <div class="fTittle">
+            <h1 class="cTittle">Apex Skrin Calculator</h1>
+        </div>
+
+        <nav class="fNav">
+            <ul class="fList">
+                <li class="cList"><a href="/Apex_Skrim">Search</a></li>
+                <li class="cList"><a href="/Apex_Skrim">Popular</a></li>
+                <li class="cList"><a href="/Apex_Skrim">Register</a></li>
+                <li class="cList"><a href="/Apex_Skrim">My Account</a></li>
+
+            </ul>
+        </nav>
+    </header>
+    
+</body>
+</html>
