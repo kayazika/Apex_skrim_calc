1a7bffec664c2b1e1db505ec2649027acd48c657
kaya <alexkayaalmeida@gmail.com>
2022-10-22 15:34:44 -0300

    start rank system

--- painel.php
+++ painel.php
@@ -29,6 +29,36 @@ include('login_verify.php');
             </ul>
         </nav>
     </header>
+
+    <main>
+       <h2>You Leaderboard</h2>
+       <table class=""box-table>
+            <tr>
+                <td>Rank</td>
+                <td>Team</td>
+                <td>Kill</td>
+                <td>Points</td>
+            </tr>
+            <?php 
+            $con = mysqli_connect("localhost", "root", "","liderboard");
+
+            $result = mysqli_query($con, "SELECT * FROM `pbo_beta` ORDER BY points DESC, kills DESC");
+
+            $ranking = 1;
+
+            if (mysqli_num_rows($result)) {
+                while ($row = mysqli_fetch_array($result)) {
+                    echo "<td>{$ranking}</td>
+                    <td>{$row['team']}</td>
+                    <td>{$row['kills']}</td>
+                    <td>{$row['points']}</td><tr>";
+                    $ranking++;
+                    
+                }
+            }
+            ?>
+        </table>
+    </main>
     
 </body>
 </html>
